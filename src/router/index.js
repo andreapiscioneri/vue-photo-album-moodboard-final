3a61c04371a1e5c4807d1b3e2d739c1864c5e90d
andreapiscioneri <andrypiscioneri@gmail.com>
2025-05-09 15:53:16 +0200

    fix: correggi la formattazione del codice e migliora la leggibilità nel router

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Andrea from '@/components/andrea.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Andrea from '@/components/andrea.vue'; // Importa il componente
 
 const routes = [
-  { path: '/andrea', name: 'Andrea', component: Andrea },
-]
+  {
+    path: '/andrea',
+    name: 'Andrea',
+    component: Andrea
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
-})
+  routes
+});
 
-export default router
+export default router;
